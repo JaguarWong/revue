2714e1a0f28d6bd3998876df77f8c59b80d4fe46
Jeff Hickman <jeffrosshickman@gmail.com>
2024-08-14 14:27:08 -0700

    basic querying works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from './firebase'; // Adjust the import path as needed
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>Hello</p>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    // Function to fetch the document
+    const fetchTitle = async () => {
+      // Replace 'movies' with your collection name and 'docId' with your document ID
+      const docRef = doc(db, 'movies', 'jrD59zATINV4dSUR6dKX');
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setTitle(docSnap.data().title);
+      } else {
+        console.log('No such document!');
+      }
+    };
+
+    fetchTitle();
+  }, []);
+
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div>
+      <h1>{title ? title : 'Loading...'}</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEiyPDMzfiB_P1BF-AMZVOlckovWkAFtU",
+    authDomain: "revue-8f031.firebaseapp.com",
+    projectId: "revue-8f031",
+    storageBucket: "revue-8f031.appspot.com",
+    messagingSenderId: "980401304540",
+    appId: "1:980401304540:web:ec75a3c4ad2403fdc2d519",
+    measurementId: "G-3WSSFXY9GR"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
